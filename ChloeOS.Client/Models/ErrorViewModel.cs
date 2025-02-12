b0d13e9155209c620f751a9ad2a5495c1f4fc9c7
Marc-André Aumont <Aumont.MarcAndre@cegep-heritage.qc.ca>
2025-02-11 21:15:17 -0500

    Initial solution setup

---
+++ ChloeOS.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace ChloeOS.Client.Models;
+
+public class ErrorViewModel {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
