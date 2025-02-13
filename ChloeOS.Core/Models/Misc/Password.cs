1d2b4bd7d1bf30ec10fd1640477072574d260346
Marc-André Aumont <Aumont.MarcAndre@cegep-heritage.qc.ca>
2025-02-12 19:12:02 -0500

    Rewrote hashing algorithm and verification

--- ChloeOS.Core/Models/Misc/Password.cs
+++ ChloeOS.Core/Models/Misc/Password.cs
@@ -10,8 +10,8 @@ public class Password {
     private const int Iterations = 100000;
     private static readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA256;
 
-    private byte[] Hash { get; set; }
-    private byte[] Salt { get; set; }
+    public byte[] Hash { get; }
+    public byte[] Salt { get; }
 
     public Password(byte[] hash, byte[] salt) {
         Hash = hash;
@@ -21,17 +21,30 @@ public class Password {
     public Password(string hash, string salt) : this(Convert.FromBase64String(hash), Convert.FromBase64String(salt)) {
     }
 
+    // User --> Password.
+    public static explicit operator Password(User user) => new (user.PasswordHash, user.PasswordSalt);
+
     public static Password GenerateHash(string? password) {
         if (string.IsNullOrEmpty(password)) {
             return null!;
         }
 
         byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
-        byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, Algorithm, HashSize);
+        byte[] hash = GetHash(password, salt);
 
         return new Password(hash, salt);
     }
 
-    public bool Verify(Password password) => CryptographicOperations.FixedTimeEquals(Hash, password.Hash);
+    public bool VerifyHash(string password) {
+        byte[] hash = GetHash(password, Salt);
+
+        string hashString = Convert.ToBase64String(hash) + Convert.ToBase64String(Salt);
+        return hash.SequenceEqual(Hash) && ToString() == hashString;
+    }
+
+    private static byte[] GetHash(string password, byte[] salt)
+        => Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, Algorithm, HashSize);
+
+    public override string ToString() => Convert.ToBase64String(Hash) + Convert.ToBase64String(Salt);
 
 }
