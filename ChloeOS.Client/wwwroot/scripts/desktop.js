ffe1d36bde5250caee8dd7b3b7622bc493ac197b
Marc-André Aumont <Aumont.MarcAndre@cegep-heritage.qc.ca>
2025-02-14 01:02:37 -0500

    Added Welcome screen onto Desktop after sign-in.

--- ChloeOS.Client/wwwroot/scripts/desktop.js
+++ ChloeOS.Client/wwwroot/scripts/desktop.js
@@ -1,4 +1,19 @@
 ﻿// -----------------------------------------------------------------------
+// Welcome screen.
+const $welcome = $(`#welcome`);
+
+if (window.sessionStorage.getItem(`welcomed`)) {
+    $welcome.remove();
+} else {
+    const startupSound = new Audio(`../sounds/syson.wav`);
+    startupSound.play();
+    $welcome.on(`animationend`, function () {
+        window.sessionStorage.setItem(`welcomed`, `true`);
+        $(this).remove();
+    });
+}
+
+// -----------------------------------------------------------------------
 // Selection rectangle.
 
 const $desktopArea = $(`main`);
